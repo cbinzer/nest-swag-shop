f1b84a7d1b5001473ef33ef0f1d4d33e9fad27fa
Christian Binzer <christian.binzer@codecentric.de>
2019-03-23 11:34:37 +0100

    refactor(swag): Remove unused app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { SwagService } from './swag/swag.service';
-import { SwagController } from './swag/swag.controller';
 import { SwagModule } from './swag/swag.module';
 
 @Module({
-  imports: [SwagModule],
-  controllers: [AppController, SwagController],
-  providers: [AppService, SwagService],
+  imports: [
+    SwagModule
+  ]
 })
 export class AppModule {}
